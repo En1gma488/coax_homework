4e3ad1d4798943e00ded9ec95e3eedb3110bcc13
En1gma488 <eigmo@mail.ru>
2019-04-12 14:56:27 +0300

    Update news_parser_spec.rb

--- spec/news_parser_spec.rb
+++ spec/news_parser_spec.rb
@@ -4,13 +4,13 @@ require_relative "../app/httparty"
 describe ParserNews do 
   let(:parser) {ParserNews.country_news('us')}
   	it 'create instance of class ParserNews' do
-  		expect(parser).to be_an_instance_of(ParserNews)
+  	  expect(parser).to be_an_instance_of(ParserNews)
   	end
-		it 'response in varible_data' do 
-			expect(parser.var).not_to eq(nil)
-		end
-		it 'save response to file' do
-			parser.save_file
-			expect(File.exist? 'ResponseFile.json').to eq(true )
-		end
+	it 'response in varible_data' do 
+	  expect(parser.var).not_to eq(nil)
+	end
+	it 'save response to file' do
+	  parser.save_file
+	  expect(File.exist? 'ResponseFile.json').to eq(true )
+	end
 end	
